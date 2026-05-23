d240afa169f1772b43f48d49c3558ba13f8b1527
8irdsand8ees <vonnightly0@gmail.com>
2026-05-23 14:27:47 -0600

    Add note saving feature

--- main.py
+++ main.py
@@ -11,12 +11,23 @@ def file_list():
    print(os.listdir("."))
 
 
+def create_note():
+
+    note =input("Write your note: ")
+
+    with open("notes.txt", "a") as file:
+        file.write(note + "\n")
+
+    print("Note saved")
+
+
 while True:
 
     print("\nTool Kit v0.3")
     print("1. System Info")
     print("2. File List")
-    print("3. Exit")
+    ptint("3. Create Note")
+    print("4. Exit")
 
     choice =  input("> ")
 
@@ -27,6 +38,9 @@ while True:
         file_list()
 
     elif choice == "3":
+        create_note()
+
+    elif choice == "4":
         print("Exiting...")
         break
 
