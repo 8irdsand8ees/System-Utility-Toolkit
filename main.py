ddb2fb18278c00e2607502409f8fb5e562622bab
8irdsand8ees <vonnightly0@gmail.com>
2026-05-23 13:45:55 -0600

    Add looping menu system

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Tool Kit v0.1 running")
+import platform
+import os
+
+while True:
+
+    print("\nTool Kit v0.2")
+    print("1. System Info")
+    print("2. File List")
+    print("3. Exit")
+
+    choice =  input("> ")
+
+    if choice == "1":
+        print(platform.system())
+        print(platform.version())
+
+    elif choice =="2":
+        print(os.listdir("."))
+
+    elif choice == "3":
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid option")
